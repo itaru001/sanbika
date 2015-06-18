f73ae5b277d2206b5ff16707c08fa10caf40d93d
Gabriel A Zayas <g-zayas@hotmail.com>
2015-06-18 19:16:55 +0900

    「死を打ち破り」の追加や.icoのpathの変更

---
+++ Files/Songs/ShiWoUchiYaburi.js
@@ -0,0 +1,106 @@
+
+
+function Jissou (uta) {
+
+	var chord1 = document.getElementById("chord1");
+	chord1.innerHTML = uta.getSharpC();
+
+	var chord2 = document.getElementById("chord2");
+	chord2.innerHTML = uta.getSharpG();
+
+	var chord3 = document.getElementById("chord3");
+	chord3.innerHTML = uta.getSharpD() + "m";
+
+	var chord4 = document.getElementById("chord4");
+	chord4.innerHTML = uta.getSharpF();
+
+	var chord5 = document.getElementById("chord5");
+	chord5.innerHTML = uta.getSharpC();
+
+	var chord6 = document.getElementById("chord6");
+	chord6.innerHTML = uta.getSharpG();
+
+	var chord7 = document.getElementById("chord7");
+	chord7.innerHTML = uta.getSharpD() + "m";
+
+	var chord8 = document.getElementById("chord8");
+	chord8.innerHTML = uta.getSharpF();
+
+	var chord9 = document.getElementById("chord9");
+	chord9.innerHTML = uta.getSharpA() + "m";
+
+	var chord10 = document.getElementById("chord10");
+	chord10.innerHTML = uta.getSharpG();
+
+	var chord11 = document.getElementById("chord11");
+	chord11.innerHTML = uta.getSharpF();
+
+	var chord12 = document.getElementById("chord12");
+	chord12.innerHTML = uta.getSharpG();
+
+	// サビ
+	var chord13 = document.getElementById("chord13");
+	chord13.innerHTML = uta.getSharpC();
+
+	var chord14 = document.getElementById("chord14");
+	chord14.innerHTML = uta.getSharpG();
+
+	var chord15 = document.getElementById("chord15");
+	chord15.innerHTML = uta.getSharpA() + "m";
+
+	var chord16 = document.getElementById("chord16");
+	chord16.innerHTML = uta.getSharpF();
+
+	var chord17 = document.getElementById("chord17");
+	chord17.innerHTML = uta.getSharpC();
+
+	var chord18 = document.getElementById("chord18");
+	chord18.innerHTML = uta.getSharpG();
+
+	var chord19 = document.getElementById("chord19");
+	chord19.innerHTML = uta.getSharpF();
+
+	var chord20 = document.getElementById("chord20");
+	chord20.innerHTML = uta.getSharpF() + "/" + uta.getSharpG();
+
+	var chord21 = document.getElementById("chord21");
+	chord21.innerHTML = uta.getSharpG();
+
+	var chord22 = document.getElementById("chord22");
+	chord22.innerHTML = uta.getSharpC();
+
+	// イエスは主
+	var chord23 = document.getElementById("chord23");
+	chord23.innerHTML = uta.getSharpC();
+
+	var chord24 = document.getElementById("chord24");
+	chord24.innerHTML = uta.getSharpG();
+
+	var chord25 = document.getElementById("chord25");
+	chord25.innerHTML = uta.getSharpD() + "m";
+
+	var chord26 = document.getElementById("chord26");
+	chord26.innerHTML = uta.getSharpF();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
