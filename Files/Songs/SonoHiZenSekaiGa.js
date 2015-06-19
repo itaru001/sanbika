3a6ba04a163c90abe89ccfc32ce10783c17ac06b
Gabriel A Zayas <g-zayas@hotmail.com>
2015-06-19 18:34:29 +0900

    「その日全世界が」を追加しました

---
+++ Files/Songs/SonoHiZenSekaiGa.js
@@ -0,0 +1,204 @@
+
+
+function Jissou(uta) {
+	
+	var chord1 = document.getElementById("chord1");
+	chord1.innerHTML = uta.getSharpA();
+
+	var chord2 = document.getElementById("chord2");
+	chord2.innerHTML = uta.getSharpE() + "/" + uta.getSharpGSharp();
+
+	var chord3 = document.getElementById("chord3");
+	chord3.innerHTML = uta.getSharpFSharp() + "m";
+
+	var chord4 = document.getElementById("chord4");
+	chord4.innerHTML = uta.getSharpCSharp() + "m7";
+
+	var chord5 = document.getElementById("chord5");
+	chord5.innerHTML = uta.getSharpD();
+
+	var chord6 = document.getElementById("chord6");
+	chord6.innerHTML = uta.getSharpA() + "/" + uta.getSharpCSharp();
+
+	var chord7 = document.getElementById("chord7");
+	chord7.innerHTML = uta.getSharpG();
+
+	var chord8 = document.getElementById("chord8");
+	chord8.innerHTML = uta.getSharpE();
+
+	var chord9 = document.getElementById("chord9");
+	chord9.innerHTML = uta.getSharpA();
+
+	var chord10 = document.getElementById("chord10");
+	chord10.innerHTML = uta.getSharpE() + "/" + uta.getSharpGSharp();
+
+	var chord11 = document.getElementById("chord11");
+	chord11.innerHTML = uta.getSharpFSharp() + "m";
+
+	var chord12 = document.getElementById("chord12");
+	chord12.innerHTML = uta.getSharpCSharp() + "m7";
+
+	var chord13 = document.getElementById("chord13");
+	chord13.innerHTML = uta.getSharpD();
+
+	var chord14 = document.getElementById("chord14");
+	chord14.innerHTML = uta.getSharpA() + "/" + uta.getSharpCSharp();
+
+	var chord15 = document.getElementById("chord15");
+	chord15.innerHTML = uta.getSharpG();
+
+	var chord16 = document.getElementById("chord16");
+	chord16.innerHTML = uta.getSharpE();
+
+	var chord17 = document.getElementById("chord17");
+	chord17.innerHTML = uta.getSharpA();
+
+	var chord18 = document.getElementById("chord18");
+	chord18.innerHTML = uta.getSharpE() + "/" + uta.getSharpGSharp();
+
+	var chord19 = document.getElementById("chord19");
+	chord19.innerHTML = uta.getSharpFSharp() + "m";
+
+	var chord20 = document.getElementById("chord20");
+	chord20.innerHTML = uta.getSharpGSharp() + "m7";
+
+	var chord21 = document.getElementById("chord21");
+	chord21.innerHTML = uta.getSharpD();
+
+	var chord22 = document.getElementById("chord22");
+	chord22.innerHTML = uta.getSharpA() + "/" + uta.getSharpCSharp();
+
+	var chord23 = document.getElementById("chord23");
+	chord23.innerHTML = uta.getSharpG();
+
+	var chord24 = document.getElementById("chord24");
+	chord24.innerHTML = uta.getSharpE();
+
+	var chord25 = document.getElementById("chord25");
+	chord25.innerHTML = uta.getSharpA();
+
+	var chord26 = document.getElementById("chord26");
+	chord26.innerHTML = uta.getSharpE() + "/" + uta.getSharpGSharp();
+
+	var chord27 = document.getElementById("chord27");
+	chord27.innerHTML = uta.getSharpFSharp() + "m";
+
+	var chord28 = document.getElementById("chord28");
+	chord28.innerHTML = uta.getSharpCSharp() + "m7";
+
+	var chord29 = document.getElementById("chord29");
+	chord29.innerHTML = uta.getSharpD();
+
+	var chord30 = document.getElementById("chord30");
+	chord30.innerHTML = uta.getSharpA() + "/" + uta.getSharpCSharp();
+
+	var chord31 = document.getElementById("chord31");
+	chord31.innerHTML = uta.getSharpG();
+
+	var chord32 = document.getElementById("chord32");
+	chord32.innerHTML = uta.getSharpE() + "sus4";
+
+	// サビ
+	var chord33 = document.getElementById("chord33");
+	chord33.innerHTML = uta.getSharpA();
+
+	var chord34 = document.getElementById("chord34");
+	chord34.innerHTML = uta.getSharpE() + "/" + uta.getSharpGSharp();
+
+	var chord35 = document.getElementById("chord35");
+	chord35.innerHTML = uta.getSharpFSharp() + "m";
+
+	var chord36 = document.getElementById("chord36");
+	chord36.innerHTML = uta.getSharpE();
+
+	var chord37 = document.getElementById("chord37");
+	chord37.innerHTML = uta.getSharpD();
+
+	var chord38 = document.getElementById("chord38");
+	chord38.innerHTML = uta.getSharpA() + "/" + uta.getSharpCSharp();
+
+	var chord39 = document.getElementById("chord39");
+	chord39.innerHTML = uta.getSharpB() + "m7";
+
+	var chord40 = document.getElementById("chord40");
+	chord40.innerHTML = uta.getSharpE();
+
+	var chord41 = document.getElementById("chord41");
+	chord41.innerHTML = uta.getSharpA();
+
+	var chord42 = document.getElementById("chord42");
+	chord42.innerHTML = uta.getSharpE() + "/" + uta.getSharpGSharp();
+
+	var chord43 = document.getElementById("chord43");
+	chord43.innerHTML = uta.getSharpFSharp() + "m";
+
+	var chord44 = document.getElementById("chord44");
+	chord44.innerHTML = uta.getSharpE();
+
+	var chord45 = document.getElementById("chord45");
+	chord45.innerHTML = uta.getSharpD();
+
+	var chord46 = document.getElementById("chord46");
+	chord46.innerHTML = uta.getSharpA() + "/" + uta.getSharpCSharp();
+
+	var chord47 = document.getElementById("chord47");
+	chord47.innerHTML = uta.getSharpB() + "m7";
+
+	var chord48 = document.getElementById("chord48");
+	chord48.innerHTML = uta.getSharpE();
+
+	var chord49 = document.getElementById("chord49");
+	chord49.innerHTML = uta.getSharpA();
+
+	// 王の王
+	var chord50 = document.getElementById("chord50");
+	chord50.innerHTML = uta.getSharpE();
+
+	var chord51 = document.getElementById("chord51");
+	chord51.innerHTML = uta.getSharpD();
+
+	var chord52 = document.getElementById("chord52");
+	chord52.innerHTML = uta.getSharpA();
+
+	var chord53 = document.getElementById("chord53");
+	chord53.innerHTML = uta.getSharpE();
+
+	var chord54 = document.getElementById("chord54");
+	chord54.innerHTML = uta.getSharpD();
+
+	var chord55 = document.getElementById("chord55");
+	chord55.innerHTML = uta.getSharpA();
+
+	var chord56 = document.getElementById("chord56");
+	chord56.innerHTML = uta.getSharpE();
+
+	var chord57 = document.getElementById("chord57");
+	chord57.innerHTML = uta.getSharpD();
+
+	var chord58 = document.getElementById("chord58");
+	chord58.innerHTML = uta.getSharpA();
+
+	var chord59 = document.getElementById("chord59");
+	chord59.innerHTML = uta.getSharpB() + "m7";
+
+	var chord60 = document.getElementById("chord60");
+	chord60.innerHTML = uta.getSharpD();
+
+	var chord61 = document.getElementById("chord61");
+	chord61.innerHTML = uta.getSharpE();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
