e1e2f585eaadd938187536a65d1ed09359693d8f
Gabriel A Zayas <g-zayas@hotmail.com>
2015-06-20 19:14:13 +0900

    index.html の背景を変えて、readme を変えて、「イエスが愛したように」を追加しました

---
+++ Files/Songs/IesuGaAishitaYouNi.js
@@ -0,0 +1,117 @@
+
+
+function Jissou(uta) {
+	var chord1 = document.getElementById("chord1");
+	var chord2 = document.getElementById("chord2");
+	var chord3 = document.getElementById("chord3");
+	var chord4 = document.getElementById("chord5");
+	var chord5 = document.getElementById("chord6");
+	var chord6 = document.getElementById("chord7");
+	var chord7 = document.getElementById("chord8");
+	var chord8 = document.getElementById("chord9");
+	var chord9 = document.getElementById("chord10");
+	var chord11 = document.getElementById("chord11");
+	var chord12 = document.getElementById("chord12");
+	var chord13 = document.getElementById("chord13");
+	var chord14 = document.getElementById("chord14");
+	var chord15 = document.getElementById("chord15");
+	var chord16 = document.getElementById("chord16");
+	var chord17 = document.getElementById("chord17");
+	var chord18 = document.getElementById("chord18");
+	var chord19 = document.getElementById("chord19");
+	var chord20 = document.getElementById("chord20");
+	var chord21 = document.getElementById("chord21");
+	var chord22 = document.getElementById("chord22");
+	var chord23 = document.getElementById("chord23");
+	var chord24 = document.getElementById("chord24");
+	var chord25 = document.getElementById("chord25");
+	var chord26 = document.getElementById("chord26");
+	var chord27 = document.getElementById("chord27");
+	var chord28 = document.getElementById("chord28");
+	var chord29 = document.getElementById("chord29");
+	var chord30 = document.getElementById("chord30");
+	var chord31 = document.getElementById("chord31");
+	var chord32 = document.getElementById("chord32");
+	var chord33 = document.getElementById("chord33");
+	var chord34 = document.getElementById("chord34");
+	var chord35 = document.getElementById("chord35");
+	var chord36 = document.getElementById("chord36");
+	var chord37 = document.getElementById("chord37");
+	var chord38 = document.getElementById("chord38");
+	var chord39 = document.getElementById("chord39");
+	var chord40 = document.getElementById("chord40");
+	var chord41 = document.getElementById("chord41");
+	var chord42 = document.getElementById("chord42");
+	var chord43 = document.getElementById("chord43");
+	var chord44 = document.getElementById("chord44");
+	var chord45 = document.getElementById("chord45");
+	var chord46 = document.getElementById("chord46");
+
+	chord1.innerHTML = uta.getSharpA();
+	chord2.innerHTML = uta.getSharpFSharp() + "m";
+	chord3.innerHTML = uta.getSharpCSharp() + "m"
+	chord4.innerHTML = uta.getSharpFSharp() + "m";
+	chord5.innerHTML = uta.getSharpD();
+	chord6.innerHTML = uta.getSharpE();
+	chord7.innerHTML = uta.getSharpA();
+	chord8.innerHTML = uta.getSharpA() + "7";
+	chord9.innerHTML = uta.getSharpD();
+	chord10.innerHTML = uta.getSharpE() + "/" + uta.getSharpD();
+	chord11.innerHTML = uta.getSharpCSharp() + "m";
+	chord12.innerHTML = uta.getSharpFSharp() + "m";
+	chord13.innerHTML = uta.getSharpB() + "m7";
+	chord14.innerHTML = uta.getSharpE() + "7";
+	chord15.innerHTML = uta.getSharpA();
+	chord16.innerHTML = uta.getSharpA();
+
+	chord17.innerHTML = uta.getSharpFSharp() + "m";
+	chord18.innerHTML = uta.getSharpCSharp() + "m";
+	chord19.innerHTML = uta.getSharpFSharp() + "m";
+	chord20.innerHTML = uta.getSharpD();
+	chord21.innerHTML = uta.getSharpE();
+	chord22.innerHTML = uta.getSharpA();
+	chord23.innerHTML = uta.getSharpA() + "7";
+
+	chord24.innerHTML = uta.getSharpD();
+	chord25.innerHTML = uta.getSharpE() + "/" + uta.getSharpD();
+	chord26.innerHTML = uta.getSharpCSharp() + "m";
+	chord27.innerHTML = uta.getSharpFSharp() + "m";
+	chord28.innerHTML = uta.getSharpB() + "m7";
+	chord29.innerHTML = uta.getSharpE() + "7";
+	chord30.innerHTML = uta.getSharpA();
+	chord31.innerHTML = uta.getSharpA() + "7";
+
+	// サビ
+	chord32.innerHTML = uta.getSharpD();
+	chord33.innerHTML = uta.getSharpE() + "/" + uta.getSharpD();
+	chord34.innerHTML = uta.getSharpCSharp() + "m";
+	chord35.innerHTML = uta.getSharpFSharp() + "m";
+	chord36.innerHTML = uta.getSharpB() + "m7";
+	chord37.innerHTML = uta.getSharpE();
+	chord38.innerHTML = uta.getSharpA();
+	chord39.innerHTML = uta.getSharpA() + "7";
+	chord40.innerHTML = uta.getSharpD();
+	chord41.innerHTML = uta.getSharpE() + "/" + uta.getSharpD();
+	chord42.innerHTML = uta.getSharpCSharp() + "m";
+	chord43.innerHTML = uta.getSharpFSharp() + "m";
+	chord44.innerHTML = uta.getSharpB() + "m7";
+	chord45.innerHTML = uta.getSharpE() + "7";
+	chord46.innerHTML = uta.getSharpA();
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
