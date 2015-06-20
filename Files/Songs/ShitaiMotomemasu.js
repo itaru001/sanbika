0040dc8b831480a47d55e5e6322d3ab8ce91ee9b
Gabriel A Zayas <g-zayas@hotmail.com>
2015-06-20 09:04:46 +0900

    「慕い求めます」を追加しました

---
+++ Files/Songs/ShitaiMotomemasu.js
@@ -0,0 +1,82 @@
+
+
+function Jissou(uta) {
+
+	var chord1 = document.getElementById("chord1");
+	var chord2 = document.getElementById("chord2");
+	var chord3 = document.getElementById("chord3");
+	var chord4 = document.getElementById("chord4");
+	var chord5 = document.getElementById("chord5");
+	var chord6 = document.getElementById("chord6");
+	var chord7 = document.getElementById("chord7");
+	var chord8 = document.getElementById("chord8");
+	var chord9 = document.getElementById("chord9");
+	var chord10 = document.getElementById("chord10");
+	var chord11 = document.getElementById("chord11");
+	var chord12 = document.getElementById("chord12");
+	var chord13 = document.getElementById("chord13");
+	var chord14 = document.getElementById("chord14");
+	var chord15 = document.getElementById("chord15");
+	var chord16 = document.getElementById("chord16");
+	var chord17 = document.getElementById("chord17");
+	var chord18 = document.getElementById("chord18");
+	var chord19 = document.getElementById("chord19");
+	var chord20 = document.getElementById("chord20");
+	var chord21 = document.getElementById("chord21");
+	var chord22 = document.getElementById("chord22");
+	var chord23 = document.getElementById("chord23");
+	var chord24 = document.getElementById("chord24");
+	var chord25 = document.getElementById("chord25");
+	var chord26 = document.getElementById("chord26");
+	var chord27 = document.getElementById("chord27");
+	var chord28 = document.getElementById("chord28");
+	var chord29 = document.getElementById("chord29");
+
+	chord1.innerHTML = uta.getSharpF();
+	chord2.innerHTML = uta.getSharpC() + "/" + uta.getSharpE();
+	chord3.innerHTML = uta.getSharpD() + "m";
+	chord4.innerHTML = uta.getSharpF() + "/" + uta.getSharpC();
+	chord5.innerHTML = uta.getFlatBFlat();
+	chord6.innerHTML = uta.getSharpF() + "/" + uta.getSharpA();
+	chord7.innerHTML = uta.getSharpG() + "m7";
+	chord8.innerHTML = uta.getSharpC();
+	chord9.innerHTML = uta.getSharpF();
+	chord10.innerHTML = uta.getSharpCSharp() + "dim";
+	chord11.innerHTML = uta.getSharpD() + "m";
+	chord12.innerHTML = uta.getSharpF() + "/" + uta.getSharpC();
+	chord13.innerHTML = uta.getFlatBFlat();
+	chord14.innerHTML = uta.getSharpG() + "m7/" + uta.getSharpC();
+	chord15.innerHTML = uta.getSharpC() + "7";
+	chord16.innerHTML = uta.getSharpF();
+
+	// サビ
+	chord17.innerHTML = uta.getSharpF() + "7";
+	chord18.innerHTML = uta.getFlatBFlat();
+	chord19.innerHTML = uta.getSharpF() + "/" + uta.getSharpA();
+	chord20.innerHTML = uta.getSharpG() + "m7";
+	chord21.innerHTML = uta.getSharpC() + "7";
+	chord22.innerHTML = uta.getSharpF();
+	chord23.innerHTML = uta.getSharpF() + "7";
+	chord24.innerHTML = uta.getFlatBFlat();
+	chord25.innerHTML = uta.getSharpF() + "/" + uta.getSharpA();
+	chord26.innerHTML = uta.getSharpD() + "m";
+	chord27.innerHTML = uta.getSharpG() + "m7";
+	chord28.innerHTML = uta.getSharpC() + "7";
+	chord29.innerHTML = uta.getSharpF();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
